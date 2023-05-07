d99077031d4e107848186dccc26fc8f2d57fd160
hojunin <dlsghwns@naver.com>
2023-05-07 16:53:11 +0900

    feat: 빌드 태그 추가

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,9 @@ const config = {
     defaultLocale: "ko",
     locales: ["ko"],
   },
+  scripts: [
+    "(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-KSCT6KG');",
+  ],
   headTags: [
     {
       tagName: "meta",
