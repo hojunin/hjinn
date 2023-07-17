942010eee030522f714ecd4345703d90d1838bc6
hojunin <dlsghwns@naver.com>
2023-07-17 18:11:01 +0900

    fix: 라이브러리 문서 이름 변경

---
+++ src/pages/books/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+const Books = () => {
+  return (
+    <Layout title={'책'} description="책페이지">
+      <div>Books</div>;
+    </Layout>
+  );
+};
+
+export default Books;
