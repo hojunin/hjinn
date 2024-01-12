d5c731644b054592508d1bbf1e2ffb438637dba6
hojunin <dlsghwns@naver.com>
2024-01-12 22:05:38 +0900

    clarify 적용

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,17 @@ const config = {
     defaultLocale: 'ko',
     locales: ['ko'],
   },
+  scripts: [
+    {
+      src: `<script type="text/javascript">
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "kkp56cyvkt");
+  </script>`,
+    },
+  ],
   headTags: [
     {
       tagName: 'meta',
