c26207ee92379b131889eb1eb0d16277a267384d
hojunin <dlsghwns@naver.com>
2024-01-12 22:11:35 +0900

    clarify 적용 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -25,13 +25,13 @@ const config = {
   },
   scripts: [
     {
-      src: `<script type="text/javascript">
+      src: `
       (function(c,l,a,r,i,t,y){
           c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
           t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
           y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
       })(window, document, "clarity", "script", "kkp56cyvkt");
-  </script>`,
+  `,
     },
   ],
   headTags: [
